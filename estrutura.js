c4b56ddafeb4a91fb5230b0301fda3792003faf4
22MathRosa-hub <matheushenriquefrosa0910@gmail.com>
2026-06-07 22:44:45 -0300

    Início

---
+++ estrutura.js
@@ -0,0 +1,17 @@
+function login() {
+
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+
+    if (user !== "" && pass !== "") {
+
+        document.getElementById("login-page").style.display = "none";
+
+        document.getElementById("map-page").style.display = "flex";
+
+    } else {
+
+        alert("Preencha usuário e senha.");
+
+    }
+}
