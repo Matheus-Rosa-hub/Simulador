4bbd39d2b1195c19c0248d8085998949d7e89625
22MathRosa-hub <matheushenriquefrosa0910@gmail.com>
2026-06-17 21:11:08 -0300

    senha

--- estrutura.js
+++ estrutura.js
@@ -3,7 +3,7 @@ function login() {
     const user = document.getElementById("username").value;
     const pass = document.getElementById("password").value;
 
-    if (user === "AAAA" && pass === "aaaa") {
+    if (user === "Equipe" && pass === "3102") {
 
         document.getElementById("login-page").style.display = "none";
         document.getElementById("map-page").style.display = "block";
